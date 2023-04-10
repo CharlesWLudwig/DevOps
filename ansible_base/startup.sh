e415a69d3d90d485b819c1cc0b923e291783ab01
Charles Ludwig <charleswludwig@gmail.com>
2023-04-10 12:17:58 -0400

    updating folder hierarchy

---
+++ ansible_base/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+service ssh start
+
+cd ansible-challenge/ansible_files/build_local_Dockerfile
+
+ansible-galaxy collections install community.docker
+
+ansible-playbook -i inventory.ini build.yml
+
+docker image ls
+
+ansible-playbook -i inventory.ini save.yml
+
+ansible-playbook -i inventory.ini load.yml
