6dec812c6374b5b1f86bbc3fac9b04c4577a0d12
Charles Ludwig <charleswludwig@gmail.com>
2023-04-11 15:05:22 -0400

    updating hierarchy

--- ansible_base/startup.sh
+++ ansible_base/startup.sh
@@ -4,8 +4,8 @@ service ssh start
 
 ansible-galaxy collections install community.docker
 
-ansible-playbook -i inventory.ini build.yml
+ansible-playbook -i inventory.ini ansible_base/build.yml
 
-ansible-playbook -i inventory.ini save.yml
+ansible-playbook -i inventory.ini ansible_base/save.yml
 
-ansible-playbook -i inventory.ini load.yml
+ansible-playbook -i inventory.ini ansible_base/load.yml
