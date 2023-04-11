c47055b636e01935a6e843cd4fea9be4a1526d9c
Charles Ludwig <charleswludwig@gmail.com>
2023-04-10 22:41:25 -0400

    updating files

--- ansible_base/startup.sh
+++ ansible_base/startup.sh
@@ -7,7 +7,3 @@ cd ../ansible_files
 ansible-galaxy collections install community.docker
 
 ansible-playbook -i inventory.ini build.yml
-
-ansible-playbook -i inventory.ini save.yml
-
-ansible-playbook -i inventory.ini load.yml
