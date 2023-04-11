efdf19f4ab2f9fcf02cd73923796d34ac36a10b3
Charles Ludwig <charleswludwig@gmail.com>
2023-04-11 17:03:19 -0400

    updating file snytax

--- startup.sh
+++ startup.sh
@@ -4,10 +4,8 @@ service ssh start
 
 ansible-galaxy collections install community.docker
 
-ansible-playbook -i inventory.ini build.yml
+ansible-playbook -i inventory.ini ./build.yml
 
-docker image ls
+ansible-playbook -i inventory.ini ./save.yml
 
-ansible-playbook -i inventory.ini save.yml
-
-ansible-playbook -i inventory.ini load.yml 
+ansible-playbook -i inventory.ini ./load.yml 
