6250631f62810e3bc050164b74402a199570731e
Charles Ludwig <charleswludwig@gmail.com>
2023-04-11 16:19:06 -0400

    updating folder hierarchy

--- startup.sh
+++ startup.sh
@@ -6,6 +6,8 @@ ansible-galaxy collections install community.docker
 
 ansible-playbook -i inventory.ini build.yml
 
+docker image ls
+
 ansible-playbook -i inventory.ini save.yml
 
-ansible-playbook -i inventory.ini load.yml
+ansible-playbook -i inventory.ini load.yml 
